7a9d51eb3233cd10b7d51efb6a2d312afba54680
rymbaniali <rym.ayasrah@hotmail.com>
2021-04-17 20:15:10 +0300

    added cart functon and page

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ let carts = document.querySelectorAll('.add-cart');
 
 let productsArr = [
 
-  { name: 'light1', price: 15.25 , inCart: 0 },
-  { name: 'light2', price: 17.55 , inCart: 0 },
-  { name: 'light3', price: 19.90 , inCart: 0 },
-  { name: 'light4', price: 21.90 , inCart: 0 },
-  { name: 'light5', price: 26.25 , inCart: 0 },
-  { name: 'light6', price: 16.70 , inCart: 0 },
-  { name: 'light7', price: 17.25 , inCart: 0 },
-  { name: 'light8', price: 16.80 , inCart: 0 },
-  { name: 'light9', price: 13.75 , inCart: 0 },
-  { name: 'light10', price: 12.90 , inCart: 0 }
+  { name: 'light1', price: 15.25, inCart: 0 },
+  { name: 'light2', price: 17.55, inCart: 0 },
+  { name: 'light3', price: 19.90, inCart: 0 },
+  { name: 'light4', price: 21.90, inCart: 0 },
+  { name: 'light5', price: 26.25, inCart: 0 },
+  { name: 'light6', price: 16.70, inCart: 0 },
+  { name: 'light7', price: 17.25, inCart: 0 },
+  { name: 'light8', price: 16.80, inCart: 0 },
+  { name: 'light9', price: 13.75, inCart: 0 },
+  { name: 'light10', price: 12.90, inCart: 0 }
 
 ];
 
@@ -68,20 +68,20 @@ function setItems(product) {
   console.log(cartItems);
 
   if (cartItems != null) {
-    if (cartItems[product.name] == undefined){
-      cartItems={
+    if (cartItems[product.name] == undefined) {
+      cartItems = {
         ...cartItems,
-        [product.name]:product
+        [product.name]: product
 
-  }
-}
+      }
+    }
     cartItems[product.name].inCart += 1;
 
-}
+  }
   else {
 
     product.inCart = 1;
-    cartItems ={[product.name]: product};
+    cartItems = { [product.name]: product };
   }
 
 
@@ -90,28 +90,53 @@ function setItems(product) {
 
 
 //create fun to  sum total price 
-function total(product){
+function total(product) {
   console.log('price :', product.price);
- 
-  let cartCost=localStorage.getItem('totalCost');
-  if(cartCost!=null){
-    cartCost=parseFloat(cartCost);
-    localStorage.setItem('totalCost',cartCost + product.price);
+
+  let cartCost = localStorage.getItem('totalCost');
+  if (cartCost != null) {
+    cartCost = parseFloat(cartCost);
+    localStorage.setItem('totalCost', cartCost + product.price);
 
   }
-  else{
-    localStorage.setItem('totalCost',product.price);
+  else {
+    localStorage.setItem('totalCost', product.price);
   }
+
+}
+
+// creat display function for cart page:
+
+function displayCart() {
+
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector('.product-img');
+
+  if (cartItems && productContainer) {
+
+    productContainer.innerHTML = '';
+    Object.values(cartItems).map(item => {
+      productContainer.innerHTML += ` 
+      
+      <div class="cartt"
+      <div class ="product">
+      <ion-icon class="icon" name="close-circle-outline"></ion-icon>
+      <img src="./img/lights/${item.name}.jpg" alt="damaged picture from display">
+      <span>${item.name}</span>
+    </div>
+    <div class ="price" >${item.price}</div>
+
+    <div class ="quantity" >${item.inCart}</div>
+    </div>
+    ` 
   
+  
+  })
+
+  }
 }
 
 
-
-
-
-
-
-
-
-
+displayCart();
 productsInsideCart();
