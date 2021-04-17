b3abcdf367960a16ce066dd3549c344c9968d5af
emanmkhareez <emkhareez19@gmail.com>
2021-04-17 15:15:21 -0700

    add total

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ let carts = document.querySelectorAll('.add-cart');
 
 let productsArr = [
 
-  { name: 'light1', price: 15.25 + 'JD', inCart: 0 },
-  { name: 'light2', price: 17.55 + 'JD', inCart: 0 },
-  { name: 'light3', price: 19.90 + 'JD', inCart: 0 },
-  { name: 'light4', price: 21.90 + 'JD', inCart: 0 },
-  { name: 'light5', price: 26.25 + 'JD', inCart: 0 },
-  { name: 'light6', price: 16.70 + 'JD', inCart: 0 },
-  { name: 'light7', price: 17.25 + 'JD', inCart: 0 },
-  { name: 'light8', price: 16.80 + 'JD', inCart: 0 },
-  { name: 'light9', price: 13.75 + 'JD', inCart: 0 },
-  { name: 'light10', price: 12.90 + 'JD', inCart: 0 }
+  { name: 'light1', price: 15.25 , inCart: 0 },
+  { name: 'light2', price: 17.55 , inCart: 0 },
+  { name: 'light3', price: 19.90 , inCart: 0 },
+  { name: 'light4', price: 21.90 , inCart: 0 },
+  { name: 'light5', price: 26.25 , inCart: 0 },
+  { name: 'light6', price: 16.70 , inCart: 0 },
+  { name: 'light7', price: 17.25 , inCart: 0 },
+  { name: 'light8', price: 16.80 , inCart: 0 },
+  { name: 'light9', price: 13.75 , inCart: 0 },
+  { name: 'light10', price: 12.90 , inCart: 0 }
 
 ];
 
@@ -24,6 +24,7 @@ for (let i = 0; i < carts.length; i++) {
 
   carts[i].addEventListener('click', () => {
     cartsNum(productsArr[i]);
+    total(productsArr[i]);
 
   });
 
@@ -86,5 +87,31 @@ function setItems(product) {
 
   localStorage.setItem('productsInCart', JSON.stringify(cartItems));
 }
+
+
+//create fun to  sum total price 
+function total(product){
+  console.log('price :', product.price);
+ 
+  let cartCost=localStorage.getItem('totalCost');
+  if(cartCost!=null){
+    cartCost=parseFloat(cartCost);
+    localStorage.setItem('totalCost',cartCost + product.price);
+
+  }
+  else{
+    localStorage.setItem('totalCost',product.price);
+  }
+  
+}
+
+
+
+
+
+
+
+
+
 
 productsInsideCart();
