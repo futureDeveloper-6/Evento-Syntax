760a208a54739e294bedc279da8696d91aa8d65b
motasimalazzam <motasim.h.alazzam@gmail.com>
2021-04-17 16:43:30 +0300

    fix the problem of repeat

--- js/form.js
+++ js/form.js
@@ -2,26 +2,35 @@
 
 // <<< create form object >>>
 
-let formArray=[];
-console.log(formArray);
-let dataArr=[];
-
 function Service(companyName, serviceName, price) {
-
     this.companyName = companyName;
     this.serviceName = serviceName;
     this.price = price;
-    formArray.push(this);
     Service.allService.push(this);
+    updateStorage();
 }
 Service.allService = [];
 console.log(Service.allService);
 
 
+// function for update data
+
+function updateStorage() {
+    let arrayString = JSON.stringify(Service.allService);
+    localStorage.setItem('Service', arrayString);
+}
+
+function getServicesdata() {
+    let data = localStorage.getItem('Service');
+    let servicetData = JSON.parse(data);
+    if (servicetData !== null) {
+        Service.allService = servicetData;
+    }
+    renderService();
+}
+
 // <<<< Render >>>>
 
-
-// add test
 // create list
 let theResults = document.getElementById('theResults');
 let theFormList = document.createElement('ul');
@@ -29,15 +38,13 @@ theResults.appendChild(theFormList);
 
 //the render function
 function renderService() {
+    theFormList.textContent = '';
     for (let i = 0; i < Service.allService.length; i++) {
-             let list = document.createElement('li');
+        let list = document.createElement('li');
+        list.textContent = `The company name: ${Service.allService[i].companyName}  The service: ${Service.allService[i].serviceName}    The price: ${Service.allService[i].price} JD`;
         theFormList.appendChild(list);
-        list.textContent = `The company name is ${Service.allService[i].companyName} and the service is ${Service.allService[i].serviceName} and the price is ${Service.allService[i].price} JD`;
-    //Service.allService=[];    
     }
-       
 }
-
 
 // <<<< add service >>>
 
@@ -49,28 +56,10 @@ serviceForm.addEventListener('submit', newService);
 //create new service
 function newService(event) {
     event.preventDefault();
-     let companyName = event.target.companyField.value;
+    let companyName = event.target.companyField.value;
     let seerviceName = event.target.serviceField.value;
     let price = event.target.priceField.value;
     new Service(companyName, seerviceName, price);
-    renderService();
-    updateStorage();
-}
-renderService();
-
-// function for update data
-
-function updateStorage(){
-    let arrayString=JSON.stringify(Service.allService);
-    localStorage.setItem('Service',arrayString);
-}
-
-function getServicesdata(){
-    let data=localStorage.getItem('Service');
-    let servicetData=JSON.parse(data);
-    if(servicetData!==null){
-        Service.allService=servicetData;
-    }
     renderService();
 }
 getServicesdata();
