3c593b5e6a97fe1f748af4eda255f93a369c0e5e
Haneenabonser <aabonser@gmail.com>
2021-04-19 00:15:43 +0300

    finished form

--- js/form.js
+++ js/form.js
@@ -29,69 +29,43 @@ console.log(Service.allService);
 
 // add test
 // create table
-let theResults = document.getElementById('theResults');
-let theFormTable = document.createElement('table');
-theResults.appendChild(theFormTable);
+
 
 //the render function
 // create header array
-let headerArray = ['Company']
+// let headerArray = ['Company Name', 'Service Name', 'Price', 'Contact', 'URl Filed', 'Description'];
 
-function header() {
-    let headingRow = document.createElement('tr');
-    theFormTable.appendChild(headingRow);
-    let thElement = document.createElement('th');
-    headingRow.appendChild(thElement);
-    thElement.textContent = "Company Name";
-    for (let i = 0; i < 7; i++) {
-        let hElement = document.createElement('th');
-        headingRow.appendChild(hElement);
-        hElement.textContent = hoursOpen[i];
-    }
-    let dailyTotalElement = document.createElement('th');
-    headingRow.appendChild(dailyTotalElement);
-    dailyTotalElement.textContent = "Daily Location Total";
-};
-
-function renderService() {
-
-    theFormTable.textContent = "";
-    for (let i = 0; i < Service.allService.length; i++) {
-        let row = document.createElement('tr');
-        theFormTable.appendChild(row);
-        row.textContent = `The company name  : ${Service.allService[i].companyName}\n  the service :  ${Service.allService[i].serviceName} the price ( ${Service.allService[i].price} JD \r the phone contact: ${Service.allService[i].contact}  \r the url for service : ${Service.allService[i].urlFiled}   brife description for service : ${Service.allService[i].description} )`;
-        //Service.allService=[];    
-    }
-}
-
-// create list
+// function header() {
+//     let headingRow = document.createElement('tr');
+//     theFormTable.appendChild(headingRow);
+//     for (let i = 0; i < headerArray.length; i++) {
+//         let hElement = document.createElement('th');
+//         headingRow.appendChild(hElement);
+//         hElement.textContent = headerArray[i];
+        
+//     }
+// }
 
 
 let theResults = document.getElementById('theResults');
-// let theFormList = document.createElement('ul');
-// theResults.appendChild(theFormList);
+
+function renderService() {
+    theResults.textContent = "";
+    for (let i = 0; i < Service.allService.length; i++) {
+        let divBox = document.createElement('div');
+        theResults.appendChild(divBox);
+        divBox.id ="divBox";
+        divBox.textContent = `The company name  : ${Service.allService[i].companyName}\n  the service :  ${Service.allService[i].serviceName} the price ( ${Service.allService[i].price} JD \r the phone contact: ${Service.allService[i].contact}  \r the url for service : ${Service.allService[i].urlFiled}   brife description for service : ${Service.allService[i].description} )`;
+    }
+   
+}
+
+renderService();
 
 
-// for (let i = 0; i < formArray.length ; i++) {
-//     theFormList = "";
-//     renderService();
-//     //the render function
-// }
 
-// function renderService() {
-//     theResults.textContent = "";
-//     for (let i = 0; i < Service.allService.length; i++) {
-//         var divigin = document.createElement('div');
-//         theResults.appendChild(divigin);
-//         divigin.textContent = `The company name  : ${Service.allService[i].companyName}\n  the service :  ${Service.allService[i].serviceName} the price ( ${Service.allService[i].price} JD \r the phone contact: ${Service.allService[i].contact}  \r the url for service : ${Service.allService[i].urlFiled}   brife description for service : ${Service.allService[i].description} )`;
-//         //Service.allService=[];    
-//     }
 
-// divigin.id ="divBox";
-// document.getElementById('divBox').style.border = "5px" ;
     
-//     //\n
-// }
 
 
 
@@ -117,6 +91,7 @@ function newService(event) {
 
 
     new Service(companyName, serviceName, price, phone, link, description);
+   
     renderService();
 
     updateStorage();
