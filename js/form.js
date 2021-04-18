934396cf4d065e50277ebc6906204097c1613472
Haneenabonser <aabonser@gmail.com>
2021-04-18 21:56:21 +0300

    table

--- js/form.js
+++ js/form.js
@@ -2,9 +2,9 @@
 
 // <<< create form object >>>
 
-let formArray=[];
+let formArray = [];
 console.log(formArray);
-let dataArr=[];
+let dataArr = [];
 
 function Service(companyName, serviceName, price) {
 
@@ -22,23 +22,42 @@ console.log(Service.allService);
 
 
 // add test
-// create list
+// create table
 let theResults = document.getElementById('theResults');
-let theFormList = document.createElement('ul');
-theResults.appendChild(theFormList);
+let theFormTable = document.createElement('table');
+theResults.appendChild(theFormTable);
 
 //the render function
+// create header array
+let headerArray = ['Company']
+
+function header() {
+    let headingRow = document.createElement('tr');
+    theFormTable.appendChild(headingRow);
+    let thElement = document.createElement('th');
+    headingRow.appendChild(thElement);
+    thElement.textContent = "Company Name";
+    for (let i = 0; i < 7; i++) {
+        let hElement = document.createElement('th');
+        headingRow.appendChild(hElement);
+        hElement.textContent = hoursOpen[i];
+    }
+    let dailyTotalElement = document.createElement('th');
+    headingRow.appendChild(dailyTotalElement);
+    dailyTotalElement.textContent = "Daily Location Total";
+};
+
 function renderService() {
 
-    theFormList.textContent="";
+    theFormTable.textContent = "";
     for (let i = 0; i < Service.allService.length; i++) {
-             let list = document.createElement('li');
-        theFormList.appendChild(list);
-        list.textContent = `The company name is : ${Service.allService[i].companyName} and the service : is ${Service.allService[i].serviceName} and the price ( ${Service.allService[i].price} JD)`;
-    //Service.allService=[];    
+        let row = document.createElement('tr');
+        theFormTable.appendChild(row);
+        row.textContent = `The company name is : ${Service.allService[i].companyName} and the service : is ${Service.allService[i].serviceName} and the price ( ${Service.allService[i].price} JD)`;
+        //Service.allService=[];    
     }
-    
-    
+
+
 }
 
 
@@ -55,7 +74,7 @@ serviceForm.addEventListener('submit', newService);
 //create new service
 function newService(event) {
     event.preventDefault();
-     let companyName = event.target.companyField.value;
+    let companyName = event.target.companyField.value;
     let seerviceName = event.target.serviceField.value;
     let price = event.target.priceField.value;
     new Service(companyName, seerviceName, price);
@@ -66,16 +85,16 @@ renderService();
 
 // function for update data
 
-function updateStorage(){
-    let arrayString=JSON.stringify(Service.allService);
-    localStorage.setItem('Service',arrayString);
+function updateStorage() {
+    let arrayString = JSON.stringify(Service.allService);
+    localStorage.setItem('Service', arrayString);
 }
 
-function getServicesdata(){
-    let data=localStorage.getItem('Service');
-    let servicetData=JSON.parse(data);
-    if(servicetData!==null){
-        Service.allService=servicetData;
+function getServicesdata() {
+    let data = localStorage.getItem('Service');
+    let servicetData = JSON.parse(data);
+    if (servicetData !== null) {
+        Service.allService = servicetData;
     }
     renderService();
 }
